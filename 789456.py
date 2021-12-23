4cec858402495c13e931aaa2ba367114e96a74f9
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-23 21:44:49 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -7,7 +7,7 @@ def play():
   while 1:
     #b=st.number_input("請輸入%d到%d之間的整數:"%(start,end)))
     st.write("請輸入%d到%d之間的整數:", start, end)
-    b=st.number_input("請輸整數:")
+    b=st.number_input("請輸整數:", 1)
     if b==a:
       st.write("恭喜你中獎了")
       break
