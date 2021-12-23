c0e3dbfa52f04ca4430e1b423cbbb6e90466fa01
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-23 21:21:12 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -1,10 +1,11 @@
 __author__="teacher"
 import random
+st streamlit as st 
 def paly():
   a=random.randint(2,99)
   start,end=1,100
   while 1:
-    b=int(input("請輸入%d到%d之間的整數:"%(start,end)))
+    b=int(st.number_input("請輸入%d到%d之間的整數:"%(start,end)))
     if b==a:
       st.write("恭喜你中獎了")
       break
