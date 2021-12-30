f247099c2c1698d2ccba531453477aa4091c9de1
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-30 21:07:07 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -4,7 +4,7 @@ import streamlit as st
 confirm_input = st.button('確認產生答案')
 if confirm_input:
   a=random.randint(2,99)
-  st.session_state.a = a
+  st.session_state.c = c
   st.session_state.start = 1
   st.session_state.end = 100
   
@@ -12,9 +12,9 @@ if confirm_input:
 b=st.number_input("請輸入1到100之間的整數:")
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
-  if b==st.session_state.a:
+  if b==st.session_state.c:
     st.write("恭喜你中獎了")
-  elif b>st.session_state.a:
+  elif b>st.session_state.c:
     if b>=st.session_state.end:
       st.write("輸入不合法,請重新輸入:")
     else:
