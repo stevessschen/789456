2b146854ff925c40dd630f27a65aab90f37ce408
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-30 20:23:13 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -1,25 +1,25 @@
 __author__="teacher"
 import random
 import streamlit as st
-def play():
+confirm_input = st.button('確認產生答案')
+if confirm_input:
   a=random.randint(2,99)
-  start,end=1,100
-  while 1:
-    #b=st.number_input("請輸入%d到%d之間的整數:"%(start,end)))
-    st.write("請輸入%d到%d之間的整數:", start, end)
-    b=st.number_input("請輸整數:", 1)
-    if b==a:
-      st.write("恭喜你中獎了")
-      break
-    elif b>a:
-      if b>=end:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-        end=b
+  st.session_state.a = a
+  
+start,end=1,100
+b=st.number_input("請輸入1到100之間的整數:")
+confirm_input2 = st.button('輸入確認')
+if confirm_input2:
+  if b==a:
+    st.write("恭喜你中獎了")
+    break
+  elif b>a:
+    if b>=end:
+      st.write("輸入不合法,請重新輸入:")
     else:
-      if b<=start:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-          start=b
-if __name__=='__main__':
-  play()
+      end=b
+  else:
+    if b<=start:
+      st.write("輸入不合法,請重新輸入:")
+    else:
+      start=b
