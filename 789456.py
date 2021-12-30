66e4ec80d52196aa088817ff091b14386ab2de0d
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-30 21:39:06 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -7,18 +7,19 @@ if confirm_input:
   st.session_state.c = c
   st.session_state.start = 1
   st.session_state.end = 100
-  x=st.number_input("請輸入1到100之間的整數:")
-  confirm_input2 = st.button('輸入確認')
-  if confirm_input2:
-    if x==st.session_state.c:
-      st.write("恭喜你中獎了")
-  elif x>st.session_state.c:
-    if x>=st.session_state.end:
-      st.write("輸入不合法,請重新輸入:")
-    else:
-      st.session_state.end=x
+
+x=st.number_input("請輸入1到100之間的整數:")
+confirm_input2 = st.button('輸入確認')
+if confirm_input2:
+  if x==st.session_state.c:
+    st.write("恭喜你中獎了")
+elif x>st.session_state.c:
+  if x>=st.session_state.end:
+    st.write("輸入不合法,請重新輸入:")
   else:
-    if x<=st.session_state.start:
-      st.write("輸入不合法,請重新輸入:")
-    else:
-      st.session_state.start=x
+    st.session_state.end=x
+else:
+  if x<=st.session_state.start:
+    st.write("輸入不合法,請重新輸入:")
+  else:
+   st.session_state.start=x
