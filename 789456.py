7b2bffe01d12c67dda28c84c0267ed781da75c71
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-01 22:51:22 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -2,14 +2,14 @@ __author__="teacher"
 import random
 import streamlit as st
 confirm_input = st.button('確認產生答案')
+st.session_state.start = 1
+st.session_state.end = 100
 if confirm_input:
-  st.session_state.start = 1
   st.session_state.c = c = random.randint(2,99)
-  st.session_state.end = 100
   st.session_state.start,session_state.end = 1,100
   write('c=', st.session_state.c)
 
-x=st.number_input("請輸入%g到%g之間的整數:"%(session_state.start,session_state.end))
+x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
   if x==st.session_state.c:
