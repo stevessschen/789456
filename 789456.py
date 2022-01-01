763e9b61d346fa1ebf06421332d37d92f8b80782
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-02 00:20:25 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -8,7 +8,7 @@ if confirm_input:
   st.session_state.end = end = 100
   st.session_state.start,st.session_state.end = 1,100
   st.write('c=', st.session_state.c)
-
+  
 x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
