6070bb3f94647540975676cdb61bb2f76f2de54b
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-02 00:37:20 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -15,12 +15,12 @@ if confirm_input2:
  if x==st.session_state.c:
    st.write("恭喜你中獎了")
 elif x>st.session_state.c:
- if x>=st.session_state.end:
+ if x>=end:
    st.write("輸入不合法,請重新輸入:")
  else:
-   st.session_state.end=x
+   end=x
 else:
- if x<=st.session_state.start:
+ if x<=start:
    st.write("輸入不合法,請重新輸入:")
  else:
-  st.session_state.start=x
+  start=x
