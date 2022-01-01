fc9945876489ae648ef312b7b981c7201b082ead
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-01 23:56:50 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -3,13 +3,13 @@ import random
 import streamlit as st
 confirm_input = st.button('確認產生答案')
 if confirm_input:
-  st.session_state.start = Start = 1
+  start = st.session_state.start = 1
   st.session_state.c = c = random.randint(2,99)
-  st.session_state.end = end = 100
+  end = st.session_state.end = 100
   st.session_state.start,st.session_state.end = 1,100
   st.write('c=', st.session_state.c)
 
-x=st.number_input("請輸入%g到%g之間的整數:"%(Start,end))
+x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
   if x==st.session_state.c:
@@ -20,7 +20,7 @@ elif x>st.session_state.c:
   else:
     end=x
 else:
-  if x<=Start:
+  if x<=start:
     st.write("輸入不合法,請重新輸入:")
   else:
-    Start=x
+    start=x
