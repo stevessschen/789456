8af777e771035ce2f380cabb0d255754edaaddc6
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-01 21:32:19 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -9,7 +9,7 @@ if confirm_input:
   st.session_state.end = 100
   st.write('c=', st.session_state.c)
 
-x=st.number_input("請輸入1到100之間的整數:")
+x=st.number_input("請輸入1到100之間的整數:"(steat,end))
 confirm_input2 = st.button('輸入確認')
 st.write(x)
 if confirm_input2:
