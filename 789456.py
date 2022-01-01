ec089384317da0296567cfe129f5c9a1417fbbb8
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-01 22:59:43 +0800

    Update 789456.py

--- 789456.py
+++ 789456.py
@@ -9,18 +9,18 @@ if confirm_input:
   st.session_state.start,st.session_state.end = 1,100
   st.write('c=', st.session_state.c)
 
-x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
+x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
   if x==st.session_state.c:
     st.write("恭喜你中獎了")
 elif x>st.session_state.c:
-  if x>=st.session_state.end:
+  if x>=end:
     st.write("輸入不合法,請重新輸入:")
   else:
-    st.session_state.end=x
+    end=x
 else:
-  if x<=st.session_state.start:
+  if x<=start:
     st.write("輸入不合法,請重新輸入:")
   else:
-   st.session_state.start=x
+   start=x
