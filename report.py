3ca1fcf7bec075ea51fa6477c2d8e68f446d51f2
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-06 19:41:24 +0800

    Update report.py

--- report.py
+++ report.py
@@ -9,9 +9,9 @@ if confirm_input:
  st.session_state.start,st.session_state.end = 1,100
  st.write('c=', st.session_state.c)
  
+x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
- x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
  if x==st.session_state.c:
    st.write("恭喜你中獎了")
  elif x>st.session_state.c:
