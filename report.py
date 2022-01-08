773a63e73101a89c0c28074ac2596092d61f74e8
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-08 17:42:29 +0800

    Update report.py

--- report.py
+++ report.py
@@ -52,15 +52,15 @@ else:
    st.session_state.end=x
    if x<st.session_state.c:
     if x<=st.session_state.start:
-    st.write("不合法啦,白痴:")
-    file_ = open("1.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
-    st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
-    unsafe_allow_html=True,
-    )
+     st.write("不合法啦,白痴:")
+     file_ = open("1.gif", "rb")
+     contents = file_.read()
+     data_url = base64.b64encode(contents).decode("utf-8")
+     file_.close()
+     st.markdown(
+     f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
+     unsafe_allow_html=True,
+     )
 
 else:
-    st.session_state.start=x
+     st.session_state.start=x
