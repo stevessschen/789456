3d7ed5d372c70d0a3b3f343986717c1201967ed7
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-08 16:50:09 +0800

    Update report.py

--- report.py
+++ report.py
@@ -4,7 +4,7 @@ import streamlit as st
 from PIL import Image
 st.set_page_config(
  page_title='猛祺的期末報告',
- page_icon=🙈)
+ page_icon='🙈')
 st.title('猛祺的定時炸彈')
 confirm_input = st.button('確認產生答案')
 if "start" not in st.session_state:
