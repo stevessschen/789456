5679c4e64d230e8685e3ffda1edd28266250a671
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-08 16:24:09 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 #__author__="teacher"
 import random
 import streamlit as st
+st.title(“猛祺的定時炸彈“）
 confirm_input = st.button('確認產生答案')
 if "start" not in st.session_state:
  st.session_state.start = 1
@@ -16,7 +17,6 @@ if confirm_input:
  #st.write('c=', st.session_state.c)
  
 
-st.title(“猛祺的定時炸彈“）
 x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!") 
 #confirm_input2 = 
