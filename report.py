426238bde65d18bce5a49375ad0a796c53b69a15
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-08 14:02:18 +0800

    Update report.py

--- report.py
+++ report.py
@@ -18,8 +18,8 @@ if confirm_input:
 
 x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 #st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!") 
-confirm_input2 = st.button('輸入確認')
-if confirm_input2:
+#confirm_input2 = 
+if st.button('輸入確認'):
  if x==st.session_state.c:
    st.write("恭喜你中獎了")
  elif x>st.session_state.c:
