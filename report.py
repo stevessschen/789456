223c862528133d66693fe55323624e8d12c52670
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2022-01-08 13:53:09 +0800

    Update report.py

--- report.py
+++ report.py
@@ -2,13 +2,13 @@ __author__="teacher"
 import random
 import streamlit as st
 confirm_input = st.button('確認產生答案')
+if "st.session_state.start" not in st.session_state:
+ st.session_state.start = 1
+if "st.session_state.end" not in st.session_state:
+ st.session_state.end = 100
+if "st.session_state.c" not in st.session_state:
+ st.session_state.c = random.randint(2,99)
 if confirm_input:
- if "st.session_state.start" not in st.session_state:
-  st.session_state.start = 1
- if "st.session_state.end" not in st.session_state:
-  st.session_state.end = 100
- if "st.session_state.c" not in st.session_state:
-  st.session_state.c = random.randint(2,99)
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
  st.session_state.end = 100
