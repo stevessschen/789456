91babed8ace4f62551a1e92b0f91430bcdbdd09e
stevessschen <36218346+stevessschen@users.noreply.github.com>
2022-01-09 16:17:14 +0800

    Update report.py

--- report.py
+++ report.py
@@ -13,16 +13,30 @@ if "start" not in st.session_state:
 if "end" not in st.session_state:
  st.session_state.end = 100
 if "c" not in st.session_state:
- st.session_state.c = random.randint(2,99)
+ st.session_state.c = 1
 if confirm_input:
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
  st.session_state.end = 100
  st.session_state.start,st.session_state.end = 1,100
- st.write('c=', st.session_state.c)
- 
+
+st.write('c=', st.session_state.c)
 
 x=st.sidebar.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
+st.write('x=', x)
+
+if x>st.session_state.c:
+ if x>=st.session_state.end:
+    pass
+ else:
+   st.session_state.end=x
+
+if x<st.session_state.c:
+    if x<=st.session_state.start:
+       pass
+    else:
+       st.session_state.start=x
+
 st.write("「CONFIRM」鍵記得按兩次喔,否則可能導致系統無法正常運行!") 
 #confirm_input2 = 
 #if st.sidebar.button('CONFIRM'):
