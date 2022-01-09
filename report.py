f04a5111c51a59d09335be2e1de4e6d26a48a49e
stevessschen <36218346+stevessschen@users.noreply.github.com>
2022-01-09 16:05:48 +0800

    Update report.py

--- report.py
+++ report.py
@@ -25,42 +25,42 @@ if confirm_input:
 x=st.sidebar.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 st.write("「CONFIRM」鍵記得按兩次喔,否則可能導致系統無法正常運行!") 
 #confirm_input2 = 
-if st.sidebar.button('CONFIRM'):
- if x==st.session_state.c:
-     st.subheader("核爆了吧!!!")
-     file_ = open("output_ntyylX.gif", "rb")
-     contents = file_.read()
-     data_url = base64.b64encode(contents).decode("utf-8")
-     file_.close()
-     st.markdown(
-     f'<img src="data:image/gif;base64,{data_url}" alt="output_ntyylX gif">',
-     unsafe_allow_html=True,
-     )
+#if st.sidebar.button('CONFIRM'):
+if x==st.session_state.c:
+ st.subheader("核爆了吧!!!")
+ file_ = open("output_ntyylX.gif", "rb")
+ contents = file_.read()
+ data_url = base64.b64encode(contents).decode("utf-8")
+ file_.close()
+ st.markdown(
+ f'<img src="data:image/gif;base64,{data_url}" alt="output_ntyylX gif">',
+ unsafe_allow_html=True,
+ )
 
- if x>st.session_state.c:
-   if x>=st.session_state.end:
-      st.write("不合法啦,好好選新的數字:")
-      file_ = open("1.gif", "rb")
-      contents = file_.read()
-      data_url = base64.b64encode(contents).decode("utf-8")
-      file_.close()
-      st.markdown(
-      f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
-      unsafe_allow_html=True,
-      )
-   else:
-     st.session_state.end=x
+if x>st.session_state.c:
+ if x>=st.session_state.end:
+    st.write("不合法啦,好好選新的數字:")
+    file_ = open("1.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
+    unsafe_allow_html=True,
+    )
+ else:
+   st.session_state.end=x
      
- if x<st.session_state.c:
-      if x<=st.session_state.start:
-         st.write("不合法啦,好好選新的數字:")
-         file_ = open("1.gif", "rb")
-         contents = file_.read()
-         data_url = base64.b64encode(contents).decode("utf-8")
-         file_.close()
-         st.markdown(
-         f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
-         unsafe_allow_html=True,
-         )
-      else:
-         st.session_state.start=x
+if x<st.session_state.c:
+    if x<=st.session_state.start:
+       st.write("不合法啦,好好選新的數字:")
+       file_ = open("1.gif", "rb")
+       contents = file_.read()
+       data_url = base64.b64encode(contents).decode("utf-8")
+       file_.close()
+       st.markdown(
+       f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
+       unsafe_allow_html=True,
+       )
+    else:
+       st.session_state.start=x
