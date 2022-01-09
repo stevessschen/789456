310df1ef91bf74b1ff5139cc7f5356def680c893
stevessschen <36218346+stevessschen@users.noreply.github.com>
2022-01-09 15:29:14 +0800

    Update report.py

--- report.py
+++ report.py
@@ -13,18 +13,29 @@ if "start" not in st.session_state:
 if "end" not in st.session_state:
  st.session_state.end = 100
 if "c" not in st.session_state:
- st.session_state.c = random.randint(2,99)
+ #st.session_state.c = random.randint(2,99)
+ st.session_state.c = 1
+if "x" not in st.session_state:
+ st.session_state.x = 1
+ 
 if confirm_input:
  st.session_state.c = c = random.randint(2,99)
  st.session_state.start = 1
  st.session_state.end = 100
  st.session_state.start,st.session_state.end = 1,100
+ st.session_state.x = 1
  st.write('c=', st.session_state.c)
  
+if st.session_state.x > st.session_state.c:
+  st.session_state.end=st.session_state.x
+else:
+  st.session_state.start=st.session_state.x
+  
 st.write("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
-x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
+st.session_state.x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end)) 
 st.write("「CONFIRM」鍵記得按兩次喔,否則可能導致系統無法正常運行!") 
 #confirm_input2 = 
+
 if st.button('CONFIRM'):
  if x==st.session_state.c:
      st.subheader("核爆了吧!!!")
