942e02d6348992f3ac01952e9492006523792d69
stevessschen <36218346+stevessschen@users.noreply.github.com>
2022-01-09 14:58:12 +0800

    Update report.py

--- report.py
+++ report.py
@@ -53,16 +53,16 @@ if st.button('CONFIRM'):
      #st.session_state.end=x
      
  if x<st.session_state.c:
-      st.session_state.start=x
-      if x<=st.session_state.start:
-         st.write("不合法啦,好好選新的數字:")
-         file_ = open("1.gif", "rb")
-         contents = file_.read()
-         data_url = base64.b64encode(contents).decode("utf-8")
-         file_.close()
-         st.markdown(
-         f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
-         unsafe_allow_html=True,
-         )
-      #else:
-         #st.session_state.start=x
+   st.session_state.start=x
+   if x<=st.session_state.start:
+     st.write("不合法啦,好好選新的數字:")
+     file_ = open("1.gif", "rb")
+     contents = file_.read()
+     data_url = base64.b64encode(contents).decode("utf-8")
+     file_.close()
+     st.markdown(
+     f'<img src="data:image/gif;base64,{data_url}" alt="1 gif">',
+     unsafe_allow_html=True,
+     )
+#else:
+    #st.session_state.start=x
